3f2d5839d5d487d3a445405b38de8093fb2182ee
heheli <lh20010124@163.com>
2025-08-07 17:56:15 +0800

    202508071800

---
+++ #020-事件详解/EventExample/EventExample/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Timers;
+
+namespace EventExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            Boy boy = new Boy();
+            Gril gril = new Gril();
+            timer.Elapsed += boy.Action;
+            timer.Elapsed += gril.Action;
+            timer.Start();
+            Console.ReadLine();
+        }
+    }
+
+    class Boy
+    {
+        internal void Action(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Jump!");
+        }
+    }
+
+    class Gril
+    {
+        internal void Action(object? sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Sing!");
+        }
+    }
+}
