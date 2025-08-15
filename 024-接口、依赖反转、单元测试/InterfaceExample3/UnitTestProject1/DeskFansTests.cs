107012b48770acd635011ab9349e7a55fd8465c0
heheli <lh20010124@163.com>
2025-08-15 11:32:40 +0800

    20250815

--- 024-接口、依赖反转、单元测试/InterfaceExample3/UnitTestProject1/DeskFansTests.cs
+++ 024-接口、依赖反转、单元测试/InterfaceExample3/UnitTestProject1/DeskFansTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using InterfaceExample3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace UnitTestProject1
 {
@@ -15,6 +16,34 @@ namespace UnitTestProject1
             var actual = fan.Work();
             Assert.AreEqual(expexted, actual);
         }
+        [TestMethod]
+        public void PowerHigherThan200_Warning()
+        {
+            var fan = new DeskFan(new PowerSupplyHigherThan200());
+            var expexted = "Warning!";
+            var actual = fan.Work();
+            Assert.AreEqual(expexted, actual);
+        }
+        [TestMethod]
+        public void PowerHigherThan100AndLowerThan200()
+        {
+            var mock = new Mock<IPowerSupply>();
+            mock.Setup(ps => ps.GetPower()).Returns(() => 150);
+            var fan = new DeskFan(mock.Object);
+            var expexted = "Work fine!";
+            var actual = fan.Work();
+            Assert.AreEqual(expexted, actual);
+        }
+        [TestMethod]
+        public void PowerEqual0()
+        {
+            var mock = new Mock<IPowerSupply>();
+            mock.Setup(ps => ps.GetPower()).Returns(() => 0);
+            var fan = new DeskFan(mock.Object);
+            var expexted = "Won't work";
+            var actual = fan.Work();
+            Assert.AreEqual(expexted, actual);
+        }
     }
 
     class PowerSupplyLowerThanZero : IPowerSupply
@@ -22,6 +51,14 @@ namespace UnitTestProject1
         public int GetPower()
         {
             return 0;
+        }
+    }
+
+    class PowerSupplyHigherThan200 : IPowerSupply
+    {
+        public int GetPower()
+        {
+            return 210;
         }
     }
 
